0d7c0db5cce39c3dd2228c08527f8eda5ae509d9
khanhrukachi <phamquockhanh9a9@gmail.com>
2024-06-27 10:15:36 +0700

    Refactored MainActivity01 to use ExecutorService for background tasks and alternate view positions

---
+++ app/src/main/java/com/phamquockhanh/phamquockhanh_6251071048/MainActivity01.java
@@ -0,0 +1,14 @@
+package com.phamquockhanh.phamquockhanh_6251071048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main01);
+    }
+}
